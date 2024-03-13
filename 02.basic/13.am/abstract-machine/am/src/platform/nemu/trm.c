6e5fc63070495568b90e755379b07b3e69b6ddfc
uae-1208 <a1208172292@163.com>
2024-03-13 15:36:53 +0800

    nemu batch mode; klib string.c

---
+++ 02.basic/13.am/abstract-machine/am/src/platform/nemu/trm.c
@@ -0,0 +1,30 @@
+#include <am.h>
+#include <nemu.h>
+
+extern char _heap_start;
+int main(const char *args);
+
+Area heap = RANGE(&_heap_start, PMEM_END);
+#ifndef MAINARGS
+#define MAINARGS ""
+#endif
+static const char mainargs[] = MAINARGS;
+
+void putch(char ch) {
+  outb(SERIAL_PORT, ch);
+}
+
+
+//ebreak/hit good trap : code  = 0 
+//hit bad trap  : code != 0
+void halt(int code) {
+  nemu_trap(code);
+
+  // should not reach here
+  while (1);
+}
+
+void _trm_init() {
+  int ret = main(mainargs);
+  halt(ret);
+}
